6794975052b7f3ce774d1b999dc4be0ab4f51c2a
Lefonsky <lefonsky@gmail.com>
2025-05-31 12:55:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -49,6 +49,7 @@ async def repost_handler(message: Message):
 dp.include_router(router)
 
 async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
